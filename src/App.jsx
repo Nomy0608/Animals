8abe0da36fba4f4a882bb9988ad552cddf6a9eb7
Nomy0608 <rgbnohemy@.com>
2025-04-30 13:50:41 -0700

    30_04_2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+import AnimalsShow from './AnimalsShow'
+
+function getRandomAnimal() {
+  const animals = ['cat', 'bird', 'cow', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
+function App() { 
+  const [animals, setAnimals] = useState([])
+
+  const handleClick = () =>{
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalsShow type={animal} key={index}/>
+  })
 
 
-function App() {
- 
-
-  return (
-   <>
-   <h1>Animals</h1>
-   </>
+  return(
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div className='animals-list'>
+        {renderedAnimals}
+      </div>
+    </div>
   )
 }
 
